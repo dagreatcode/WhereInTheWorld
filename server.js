8a30d2efea4f810738f5762bffa8eb0b8a8b8bef
Vincent Kendrick <dagreatcode@gmail.com>
2022-04-21 19:44:59 -0400

    ...

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("client/build"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/whereintheworld", {
   // useNewUrlParser: true,
-  // useUnifiedTopology: true,
+  useUnifiedTopology: true,
   // useCreateIndex: true,
   // useFindAndModify: false,
 });

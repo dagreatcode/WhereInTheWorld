6ba45a7988c0f002d8870ba0f0acc3252640c1a0
Vincent Kendrick <dagreatcode@gmail.com>
2022-05-12 10:25:21 -0400

    added webtoken and also bcrypt for author n authen

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,31 +1,90 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
 const db = require("../models");
+const jwt = require("jsonwebtoken");
 
 // "Sign Up"
 
-router.post("/api/signup", ({ email, password }, res) => {
+router.post("/api/signup", (req, res) => {
+  const { email, password } = req.body;
   if (!email.trim || !password.trim) {
     res.status(400);
   } else {
-    db.User.create({
-      email: email,
-      password: password,
-    })
-      .then((newUser) => {
-        res.json(newUser);
+    bcrypt
+      .hash(password, 10)
+      .then((hashedPassword) => {
+        console.log(hashedPassword);
+        db.User.create({
+          email: email,
+          password: hashedPassword,
+        })
+          .then((newUser) => {
+            res.json(newUser);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+              error: true,
+              data: null,
+              message: "Unable to signup.",
+            });
+          });
       })
       .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          error: true,
-          data: null,
-          message: "Unable to signup.",
-        });
+        res.status(500);
       });
   }
 });
 
 // "Login"
+
+router.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+  db.User.findOne({ email: email })
+    .then((foundUser) => {
+      if (foundUser) {
+        // TODO: if too many failed attempts to login.
+        bcrypt
+          .compare(password, foundUser.password)
+          .then(function (result) {
+            console.log("The password match: ", result);
+            if (result) {
+              // TODO: Send a jwt back as data instead.
+              // TODO: lock down the token in a time limit
+              // TODO: Something needs to be on the back end paying attention
+            const token = jwt.sign({ email: foundUser.email}, "shhhhh");
+              res.json({
+                err: false,
+                data: token,
+                message: "Successfully logged in.",
+              });
+            } else {
+              res.status(401).json({
+                err: true,
+                data: null,
+                message: "Failed to sign in",
+              });
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(401).json({
+              err: true,
+              data: null,
+              message: "Failed to sign in",
+            });
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        err: true,
+        data: null,
+        message: "Failed to sign in",
+      });
+    });
+});
 
 module.exports = router;

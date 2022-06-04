1be5f78b370b6b7f49835a43fab08448369c9101
Vincent Kendrick <dagreatcode@gmail.com>
2022-06-04 00:31:42 -0400

    Great Update to the UI and added the ssl and name covid-way.com ;)

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,39 +1,97 @@
 // rsc
 // rcc
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import Image from "./img10.png";
+import Image2 from "./img11.jpg";
+import Image3 from "./img14.jpg";
 
 class Home extends Component {
   render() {
     return (
       <>
         <div className="jumbotron text-center">
-          <h1 className="display-3">Welcome To The Page</h1>
-          <p className="lead">Hope you feel at home</p>
+          <h1 className="display-3">It's The Covid Way</h1>
+          <p className="lead">Search from Covid-19 new to Covid stats, and near by places to go.</p>
           <hr className="my-2" />
-          <p>More info</p>
+          <p>Stay Safe</p>
           <p className="lead">
-            <a
-              className="btn btn-primary btn-lg"
-              href="Jumbo action link"
-              role="button"
-            >
+            <Link className="btn btn-primary btn-lg" to="/Login" role="button">
               Let's Get Started
-            </a>
+            </Link>
           </p>
         </div>
-        <blockquote class="blockquote text-right">
-        <p class="mb-0"></p>
-        <footer class="blockquote-footer"> <cite title="Source Title">Source Title</cite></footer>
-      </blockquote>
-      <blockquote class="blockquote text-right">
-        <p class="mb-0"></p>
-        <footer class="blockquote-footer"> <cite title="Source Title">Source Title</cite></footer>
-      </blockquote>
-      <blockquote class="blockquote text-right">
-        <p class="mb-0"></p>
-        <footer class="blockquote-footer"> <cite title="Source Title">Source Title</cite></footer>
-      </blockquote>
-      {/* <img src="" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="placeholder"/> */}
+        <blockquote className="blockquote text-right">
+          <p className="mb-0"></p>
+          <footer className="blockquote-footer">
+            {"Covid Stats & News"}
+            <cite title="Source Title"></cite>
+            <div className="card-columns">
+              <div className="card">
+                <div className="row">
+                  <div className="col-sm-6">
+                    <div className="card-body">
+                      <h4 className="card-title">Covid</h4>
+                      <p className="card-text">
+                        Get the latest news and stats on Covid-19
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-sm-6">
+                    <img className="card-img-top" src={Image} alt="Covid button" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </footer>
+        </blockquote>
+        <br />
+        <br />
+        <blockquote className="blockquote text-right">
+          <p className="mb-0"></p>
+          <footer className="blockquote-footer">
+            {"Places Around You"}
+            <cite title="Source Title"></cite>
+            <div className="card">
+              <div className="row">
+                <div className="col-sm-6">
+                  <img className="card-img-top" src={Image2} alt="a map" />
+                </div>
+                <div className="col-sm-6">
+                  <div className="card-body">
+                    <h4 className="card-title">Where To Go</h4>
+                    <p className="card-text">
+                      Find out what type of places are near you
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </footer>
+        </blockquote>
+        <br />
+        <br />
+        <blockquote className="blockquote text-right">
+          <p className="mb-0"></p>
+          <footer className="blockquote-footer">
+            {"Coming Soon"}
+            <cite title="Source Title"></cite>
+            <div className="card">
+              <div className="row">
+                <div className="col-sm-6">
+                  <div className="card-body">
+                    <h4 className="card-title">Gas Prices</h4>
+                    <p className="card-text">Up to date Gas prices.</p>
+                  </div>
+                </div>
+                <div className="col-sm-6">
+                  <img className="card-img-top" src={Image3} alt="Stay safe smiley face" />
+                </div>
+              </div>
+            </div>
+          </footer>
+        </blockquote>
+        {/* <img src="" class="img-fluid ${3|rounded-top,rounded-right,rounded-bottom,rounded-left,rounded-circle,|}" alt="placeholder"/> */}
         {/* <div className="card text-left">
           <img className="card-img-top" src="holder.js/100px180/" alt="" />
           <div className="card-body">

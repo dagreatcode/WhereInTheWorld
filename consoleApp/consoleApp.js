5df4bda09efb018ccdd82f6fb1d077eb3a6009ff
Vincent Kendrick <dagreatcode@gmail.com>
2022-06-20 02:22:34 -0400

    lil lint

--- consoleApp/consoleApp.js
+++ consoleApp/consoleApp.js
@@ -9,68 +9,72 @@ const PORT2 = process.env.PORT || 3002;
 const app = http.createServer();
 const figlet = require("figlet");
 
-figlet('Administrator!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-	consoleApp();
+figlet("Administrator!!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+  consoleApp();
 });
 
 function consoleApp() {
-	app.listen(PORT2, () => {
-		console.log(`App is running on http://localhost:${PORT2}`);
-		handleRequest();
-	});
+  app.listen(PORT2, () => {
+    console.log(`App is running on http://localhost:${PORT2}`);
+    handleRequest();
+  });
 }
 
 function handleRequest() {
-	console.log("Hello Console App");
-	inquirer
-		.prompt([
-			{
-				type: "input",
-				message: "name please",
-				name: "username",
-			},
-		])
-		.then(function (response) {
-			figlet.text(response.username, {
-				font: 'poison',
-				horizontalLayout: 'default',
-				verticalLayout: 'default',
-				width: 80,
-				whitespaceBreak: true
-			}, function(err, data) {
-				if (err) {
-					console.log('Something went wrong...');
-					console.dir(err);
-					return;
-				}
-				console.log(data);
-			});
-			// console.log(response.username);
-			fs.appendFile("log.txt", response.username + "\n", function (err) {
-				if (err) {
-					console.log(err);
-				} else {
-					console.log("Name logged!");
-				}
-			});
-			fs.appendFile(
-				"log.json",
-				JSON.stringify(response.username) + "\n",
-				function (err) {
-					if (err) {
-						console.log(err);
-					} else {
-						console.log("Name logged!");
-					}
-				}
-			);
-		});
+  console.log("Hello Console App");
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "name please",
+        name: "username",
+      },
+    ])
+    .then(function (response) {
+      figlet.text(
+        response.username,
+        {
+          font: "poison",
+          horizontalLayout: "default",
+          verticalLayout: "default",
+          width: 80,
+          whitespaceBreak: true,
+        },
+        function (err, data) {
+          if (err) {
+            console.log("Something went wrong...");
+            console.dir(err);
+            return;
+          }
+          console.log(data);
+        }
+      );
+      // console.log(response.username);
+      fs.appendFile("log.txt", response.username + "\n", function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Name logged!");
+        }
+      });
+      fs.appendFile(
+        "log.json",
+        JSON.stringify(response.username) + "\n",
+        function (err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Name logged!");
+          }
+        }
+      );
+    });
 }
 
 module.exports = consoleApp;
